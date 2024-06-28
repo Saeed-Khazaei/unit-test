667b42adc75577befaaa21e7e84b48fdd93f371f
Saeed-Khazaei <saeedkhazaei94@google.com>
2024-06-28 09:42:37 +0330

    test: OrderStatusSelector on trigger selection items and implement parametrize unit test

--- src/tests/components/OrderStatusSelector.test.tsx
+++ src/tests/components/OrderStatusSelector.test.tsx
@@ -18,6 +18,7 @@ describe("OrderStatusSelector", () => {
       onChange,
       button: screen.getByRole("combobox"),
       user: userEvent.setup(),
+      getOption: (label: RegExp) => screen.getByRole("option", { name: label }),
       getOptions: () => screen.findAllByRole("option"), // this technique is called lazy evaluation which means it will not be evaluated until it is needed (postpone execution of it)
     };
   };
@@ -35,5 +36,44 @@ describe("OrderStatusSelector", () => {
 
     const labels = options.map((option) => option.textContent);
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
+  });
+
+  test.each([
+    {
+      label: /processed/i,
+      value: "processed",
+    },
+    {
+      label: /fulfilled/i,
+      value: "fulfilled",
+    },
+  ])(
+    "should call onChange with $value when the $label option is selected",
+    async ({ label, value }) => {
+      const { button, user, onChange, getOption } = renderComponent();
+      await user.click(button);
+
+      // const options = await getOptions();
+
+      // await user.click(options[1]);
+      // expect(onChange).toHaveBeenCalledWith("processed");
+
+      const option = await getOption(label);
+
+      await user.click(option);
+      expect(onChange).toHaveBeenCalledWith(value);
+    }
+  );
+  test("should call onChange with 'new' when the /new/i option is selected", async () => {
+    const { button, user, onChange, getOption } = renderComponent();
+    await user.click(button);
+
+    const processedOption = await getOption(/processed/i);
+    await user.click(processedOption);
+
+    await user.click(button);
+    const newOption = await getOption(/new/i);
+    await user.click(newOption);
+    expect(onChange).toHaveBeenCalledWith("new");
   });
 });
